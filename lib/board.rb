32734524ba004f6ef28e1dbcefc648eef4840888
Sankalp-G <sankalp.gawali@gmail.com>
2022-04-07 16:14:15 +0530

    implemented #create_board method

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,8 @@
 # board class for connect four
 class Board
-  def create_board; end
+  # creates a 7x6 2d array filled with blank strings
+  def create_board
+    collumn = Array.new(6, '')
+    Array.new(7, collumn)
+  end
 end

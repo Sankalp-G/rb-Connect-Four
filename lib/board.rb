b06f0b5f2260cab42a9b17c8bfd905a9005bde16
Sankalp-G <sankalp.gawali@gmail.com>
2022-04-07 16:27:12 +0530

    created #initialize for Board

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,11 @@
 # board class for connect four
 class Board
+  attr_reader :board
+
+  def initialize
+    @board = create_board
+  end
+
   # creates a 7x6 2d array filled with blank strings
   def create_board
     collumn = Array.new(6, '')

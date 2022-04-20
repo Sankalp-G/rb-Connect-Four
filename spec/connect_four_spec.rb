4411d185f2dd1c4166b2eb76b0c04f5723496510
Sankalp-G <sankalp.gawali@gmail.com>
2022-04-20 23:39:02 +0530

    created tests for #init_rounds

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/connect_four'
+require_relative '../lib/board'
 
 describe ConnectFour do
   subject(:cfour) { described_class.new }
@@ -29,6 +30,26 @@ describe ConnectFour do
         it 'returns valid input as integer' do
           expect(cfour.input_index).to eql(3)
         end
+      end
+    end
+  end
+
+  describe '#init_rounds' do
+    context 'game is won after four loops' do
+      let(:round_board) { instance_double(Board) }
+
+      before do
+        cfour.instance_variable_set(:@game_board, round_board)
+        allow(cfour).to receive(:one_round)
+        allow(round_board).to receive(:win_condition).and_return(false, false, false, false, 'blue')
+      end
+      it 'calls #one_round 4 times' do
+        expect(cfour).to receive(:one_round).exactly(4).times
+        cfour.init_rounds
+      end
+      it 'returns winning player' do
+        winning_player = 'blue'
+        expect(cfour.init_rounds).to eql(winning_player)
       end
     end
   end

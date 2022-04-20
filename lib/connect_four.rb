66b69f51ffa0e115610208e4cef22717bdeeaca1
Sankalp-G <sankalp.gawali@gmail.com>
2022-04-20 18:50:24 +0530

    created tests for #input_index

---
+++ lib/connect_four.rb
@@ -0,0 +1,5 @@
+# class used to interact with Board and handle player inputs
+class ConnectFour
+  def input_index
+  end
+end
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../lib/connect_four'
+
+describe ConnectFour do
+  subject(:cfour) { described_class.new }
+
+  describe '#input_index' do
+    context 'valid input' do
+      before do
+        valid_input = '5'
+        allow(cfour).to receive(:gets).and_return(valid_input)
+      end
+      it 'returns input as integer' do
+        expect(cfour.input_index).to eql(5)
+      end
+    end
+    context 'invalid input' do
+      context 'invalid input twice then valid input' do
+        before do
+          invalid_inputs = %w[8 0]
+          valid_input = '3'
+          allow(cfour).to receive(:gets).and_return(*invalid_inputs, valid_input)
+          allow(cfour).to receive(:puts)
+        end
+        it 'puts error twice' do
+          printed_error = "\n\nInvalid Input, input must be a number from 1 to 7\nTry Again"
+          expect(cfour).to receive(:puts).with(printed_error).twice
+          cfour.input_index
+        end
+        it 'returns valid input as integer' do
+          expect(cfour.input_index).to eql(3)
+        end
+      end
+    end
+  end
+end

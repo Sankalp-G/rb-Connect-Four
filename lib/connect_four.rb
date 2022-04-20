cb092786b56f07858c84e492c8f5ba716702ab8a
Sankalp-G <sankalp.gawali@gmail.com>
2022-04-20 19:20:04 +0530

    implemented #input_index

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,5 +1,15 @@
+require_relative 'display_print'
+
 # class used to interact with Board and handle player inputs
 class ConnectFour
+  # gets input from terminal and returns input as integer, retries if invalid
   def input_index
+    input = gets.chomp
+    raise unless input =~ /^[1-7]$/ # input must be between 1 to 7
+
+    input.to_i
+  rescue StandardError
+    DisplayPrint.input_index_error
+    retry
   end
 end

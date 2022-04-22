25c72a113f58a896feda0737e340c25072adcef5
Sankalp-G <sankalp.gawali@gmail.com>
2022-04-22 22:09:53 +0530

    created #welcome_splash_screen

--- lib/display_print.rb
+++ lib/display_print.rb
@@ -2,17 +2,30 @@ require 'colorize'
 
 # contains various console puts for ConnectFour
 module DisplayPrint
+  def self.welcome_splash_screen
+    puts <<~'HEREDOC'
+      ┏━━━┓              ┏┓ ┏━━━┓
+      ┃┏━┓┃             ┏┛┗┓┃┏━━┛
+      ┃┃ ┗┏━━┳━┓┏━┓┏━━┳━┻┓┏┛┃┗━━┳━━┳┓┏┳━┓
+      ┃┃ ┏┫┏┓┃┏┓┫┏┓┫┃━┫┏━┫┃ ┃┏━━┫┏┓┃┃┃┃┏┛
+      ┃┗━┛┃┗┛┃┃┃┃┃┃┃┃━┫┗━┫┗┓┃┃  ┃┗┛┃┗┛┃┃
+      ┗━━━┻━━┻┛┗┻┛┗┻━━┻━━┻━┛┗┛  ┗━━┻━━┻┛
+    HEREDOC
+    puts "\nEnter a number to select an option"
+    puts '[1] - Start Game'
+  end
+
+  def self.player_turn(player_color)
+    player_name = player_color.colorize(color: :black, background: player_color.to_sym)
+    puts "\n#{player_name} Turn:"
+    puts 'Enter the column number u want to drop your coin in. (1 . 7s)'
+  end
+
   def self.input_index_error
     puts "\n\nInvalid Input, input must be a number from 1 to 7\nTry Again"
   end
 
   def self.column_full_error
     puts "\n\nThat column is full, Choose a different column\n"
-  end
-
-  def self.player_turn(player_color)
-    player_name = player_color.colorize(color: :black, background: player_color.to_sym)
-    puts "\n#{player_name} Turn:"
-    puts 'Enter the column number u want to drop your coin in. (1 - 7s)'
   end
 end

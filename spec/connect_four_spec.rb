cc0e48f6f11997d2c58b8428f64f4c87ce7699b6
Sankalp-G <sankalp.gawali@gmail.com>
2022-04-25 18:50:10 +0530

    created tests for fetch_index

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -34,6 +34,43 @@ describe ConnectFour do
     end
   end
 
+  describe '#fetch_index_from_player' do
+    context 'column is not full' do
+      before do
+        valid_input = '3'
+        allow(cfour).to receive(:gets).and_return(valid_input)
+      end
+      it 'returns input as integer' do
+        expect(cfour.fetch_index_from_player).to eql(2)
+      end
+      it 'does not raise error' do
+        expect { cfour.fetch_index_from_player }.to_not raise_error
+      end
+    end
+
+    context 'column is full' do
+      context 'invalid input twice then valid input' do
+        let(:get_index_board) { instance_double(Board) }
+
+        before do
+          invalid_inputs = %w[11 apple]
+          valid_input = '3'
+          allow(cfour).to receive(:gets).and_return(*invalid_inputs, valid_input)
+          allow(DisplayPrint).to receive(:puts)
+          allow(get_index_board).to receive(:column_full?).and_return(true, true, false)
+          cfour.instance_variable_set(:@game_board, get_index_board)
+        end
+        it 'puts error twice' do
+          expect(DisplayPrint).to receive(:column_full_error).twice
+          cfour.fetch_index_from_player
+        end
+        it 'returns valid input as integer' do
+          expect(cfour.fetch_index_from_player).to eql(2)
+        end
+      end
+    end
+  end
+
   describe '#init_rounds' do
     context 'game is won after four loops' do
       let(:round_board) { instance_double(Board) }
